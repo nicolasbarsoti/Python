44458eb1d688bc449652b506f87d9b10619498f8
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-16 11:11:51 -0300

    Add files via upload

---
+++ tema 12/Ex_1_adapte_algoritmo.py
@@ -0,0 +1,16 @@
+# o cidigo ta errado
+
+A = int
+B = int 
+print (input("digite o valor da A: "), A)
+print (input("digite o valor da B: "), B)
+C = 10 , 2, 6 , 2, 15 , 3
+
+print ("valores da lista C: ", C )
+
+while A > B:
+        s = B - A   
+        b = sum(C)
+        print(" O resultado de A: ", s)
+        print(" O resultado de C: ", b)
+      
