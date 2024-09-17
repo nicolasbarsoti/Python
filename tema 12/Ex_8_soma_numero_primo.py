61177d43d88e5bf389e33d3f5a561ea53ab5adad
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-17 10:22:34 -0300

    Ex_8_soma_numero_primo.py

--- tema 12/Ex_8_soma_numero_primo.py
+++ tema 12/Ex_8_soma_numero_primo.py
@@ -1,12 +1,4 @@
-#esse codigo está completamente imcompleto
-
-for numero in range(0, 101):
-    c = 0
-    # Verifica quantos divisores o número possui
-    for i in range(1, numero + 1):
-        if numero % i == 0:
-            c += 1
-            print(i)
-    # Verifica se o número é primo
-    if c == 2:
-        print(f"{numero} é um número primo.")
+p = 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+print("Os numeros primo de 1 a 100 são: ", p)
+soma = sum  (p)
+print (" A soma dos numeros primo são: ", soma)
