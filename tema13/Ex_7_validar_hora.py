e83c10f20696a4e7f124fda3ded7cfc281bafd22
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:20:33 -0300

    Update Ex_7_validar_hora.py

--- tema13/Ex_7_validar_hora.py
+++ tema13/Ex_7_validar_hora.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 hora = input('Digite que horas são: ')
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if hora == '08:30':
     print('Hora correta!')
 else:
