d798451242b575c1274990009e74d91a01153a29
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:35:36 -0300

    Add files via upload

---
+++ Tema 14/14.calculo base triangulo.py
@@ -0,0 +1,9 @@
+# Leitura das dimensões do triângulo
+base = float(input("Digite a base do triângulo: "))
+altura = float(input("Digite a altura do triângulo: "))
+
+# Cálculo da área
+area = (base * altura)/2
+
+# Exibição do resultado
+print("A área do triângulo é:", area)
