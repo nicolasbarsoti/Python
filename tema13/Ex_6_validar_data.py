88a769271ac607f9675ee9cb449aac28444341fc
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:20:10 -0300

    Update Ex_6_validar_data.py

--- tema13/Ex_6_validar_data.py
+++ tema13/Ex_6_validar_data.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 data = int(input('Digite uma data: '))
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if data == 05112005:
     print('Data validado corretamente!')
 else:
