8fac5d42f33485bda7962dc27514046fb716ba07
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:19:09 -0300

    Update Ex_4_validar_cpf.py

--- tema13/Ex_4_validar_cpf.py
+++ tema13/Ex_4_validar_cpf.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 cpf = int(input('Digite seu CPF: '))
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if cpf == "12345678929":
     print('CPF validado corretamente!')
 else:
