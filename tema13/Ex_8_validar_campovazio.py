d11eb5818395297fd4a909ea4c2711a8adec124a
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:21:39 -0300

    Update Ex_8_validar_campovazio.py

--- tema13/Ex_8_validar_campovazio.py
+++ tema13/Ex_8_validar_campovazio.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 c = input('Digite algo: ')
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if c == '':
     print('Campo vazio validado corretamente!')
 else:
