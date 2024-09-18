f59d15e3189a3810dbf708193b0f3e8fbb425a97
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:18:42 -0300

    Update Ex_3_validar_nome.py

--- tema13/Ex_3_validar_nome.py
+++ tema13/Ex_3_validar_nome.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 n = input('Digite seu nome: ')
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if n == "Nicolas":
     print('Nome validado corretamente!')
 else:
