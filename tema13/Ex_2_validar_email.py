bef0e221e5dbf27314ac0725e24ca152de9d0717
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:18:08 -0300

    Update Ex_2_validar_email.py

--- tema13/Ex_2_validar_email.py
+++ tema13/Ex_2_validar_email.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 e = input('Digite seu email: ')
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if e == "nicolasbarsoti.nb@gmail.com":
     print('Email validado corretamente!')
 else:
