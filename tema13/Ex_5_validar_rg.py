a51cca55532f8be957c9deefcfbcac8d8e428c18
NicolasBarsoti <nicolasbarsoti.nb@gmail.com>
2024-09-18 18:19:40 -0300

    Update Ex_5_validar_rg.py

--- tema13/Ex_5_validar_rg.py
+++ tema13/Ex_5_validar_rg.py
@@ -1,5 +1,6 @@
+#aqui e criado uma variavel para que o usuario possa digitar oque for solicitado
 rg = int(input('Digite seu RG: '))
-
+#aqui e utilizado uma condicional para determinar se o valor da variavel digitada pelo usuario for igual ao valor validado e sera impresso na tela resultado correspondente
 if rg == 457682340:
     print('RG validado corretamente!')
 else:
